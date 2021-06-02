3dca663ce25ac244413682b9dc428ca85380266c
Expensure <jaspervanloon@outlook.com>
2021-06-02 12:18:06 +0200

    Plotted first towns

---
+++ get_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import csv
+
+
+
+def get_coords():
+    with open('cities_subset20.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        city_coords = []
+        for row in reader:
+            city_coords.append([float(row[1]), float(row[2])])
+        data = np.array(city_coords)
+    return data
+
+
+def plot_coords():
+    data = get_coords()
+    x, y = data.T
+    plt.scatter(x, y)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+    return None
+plot_coords()
