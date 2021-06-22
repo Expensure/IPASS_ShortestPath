3731b2b1803fed6b8141f4b220672b864829e3de
Expensure <jaspervanloon@outlook.com>
2021-06-22 14:28:24 +0200

    Put stage1 data in separate file Removed unnecessaries

--- Towns_from_data.py
+++ Towns_from_data.py
@@ -1,104 +1,67 @@
-import csv
 import Get_Score
-from Town import Town
-import Connection
+from get_data import *
 from Algorithm import algorithm
 from Clustering import cluster_data
 
-def get_data():
-    '''
-    Gets coordinate data from CSV file
-    :return: List of lists of coords
-    '''
-    with open('cities_subset20.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        city_coords = []
-        for row in reader:
-            city_coords.append([int(float(row[1])), int(float(row[2]))])
-        data = city_coords
-    return data
-
-
-def get_towns(data):
-    result = []
-    for town in data:
-        newInstance = Town(town)
-        result.append(newInstance)
-    return result
-
-
-def get_coordinates(data):
-    result = []
-    for town in data:
-        newInstance = Town(town)
-        result.append(newInstance.get_coords())
-    return result
-
-
-def get_edges(coordinates):
-    # Gets all combinations of coordinates, except combinations of itself, and gets Connection info from it
-    combos = []
-    for i in coordinates:
-        for j in coordinates:
-            if i != j:
-                combos.append([coordinates.index(i), coordinates.index(j), Connection.get_distance(i, j)])
-    return combos
-
-def getMinimum(lst,index):
+def getMinimum(lst, index):
     minimum = 10000000
     for i in lst:
         if i[index] < minimum:
             minimum = i[index]
     return minimum
 
-def getMaximum(lst,index):
+
+def getMaximum(lst, index):
     maximum = 0
     for i in lst:
         if i[index] > maximum:
             maximum = i[index]
     return maximum
 
+
 def evaluate():
-    #Oud probleem: al 100 proberen te doen zorgt voor 100! combinaties oftewel 9.33*10^157 combinaties.
-    #Nieuw plan: opsplitsen in enorme grid.
-    #Beter plan: verminder naar 150 stuks. Gebruik data learning en k-means clustering om te vereenvoudigen.
-    coords = get_coordinates(get_data())
+    # Oud probleem: al 100 proberen te doen zorgt voor 100! combinaties oftewel 9.33*10^157 combinaties.
+    # Nieuw plan: opsplitsen in enorme grid.
+    # Beter plan: verminder naar 150 stuks. Gebruik data learning en k-means clustering om te vereenvoudigen.
+    coords = get_data()
     x1 = getMinimum(coords, 0)
     x2 = getMinimum(coords, 1)
     y1 = getMaximum(coords, 0)
     y2 = getMaximum(coords, 1)
-    print((y1 - x1)**0.5) #Square root is the most efficient
-    print((y2 - x2)**0.5) #197769/(71.4*58.3) = 46.6. #47! = 2.57*10^59
+    print((y1 - x1) ** 0.5)  # Square root is the most efficient
+    print((y2 - x2) ** 0.5)  # 197769/(71.4*58.3) = 46.6. #47! = 2.57*10^59
     # dit moet nog meer vermenigvuldigt worden. ik ga alles nog delen door 2,
     # dus intel zal per (72/2 * 58/2) 36x29 pixels gaan. dan per 4 heb ik 4! mogelijkheden. en dat dan steeds uitbreiden.
+    # Andere oplossing om een aangepaste KMeans vorm toe te passen
+    # bron: https://www.analyticsvidhya.com/blog/2021/04/k-means-clustering-simplified-in-python/
     # Probleem is dat dijkstra Niet toegepast kan worden
-    #
-    dijkie = Get_Score.Score(coords=coords)
-    state = [0, 4, 2, 3, 1]
-    result, route = dijkie.evaluate(state)
-    plot_coords(coords,route)
+    # Oplossing was om eigen algoritme te maken.
+    hypothesis = Get_Score.Score(coords=coords)
+    path = [0, 4, 2, 3, 1]
+    result, route = hypothesis.evaluate(path)
+    plot_coords(coords, route)
     return result
 
 
-def plot_coords(coords,route):
+def plot_coords(coords, route):
     import matplotlib.pyplot as plt
     '''
     Plots all points in a scatterplot
     :returns nothing:
     '''
     x, y = [], []
-    for i in get_coordinates(get_data()):
+    for i in get_data():
         x.append(i[0]), y.append(i[1])
     plt.scatter(x, y)
-    xl, yl = [],[]
+    xl, yl = [], []
     for i in route:
         xl.append(coords[i][0])
         yl.append(coords[i][1])
-    plt.plot(xl,yl)
+    plt.plot(xl, yl)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
     return None
 
-print(cluster_data(get_coordinates(get_data())))
+
+print(cluster_data(get_data()))
--- get_data.py
+++ get_data.py
@@ -1,34 +1,45 @@
-import numpy as np
-from matplotlib import pyplot as plt
 import csv
 
 
-def get_coords():
+def get_data():
     '''
     Gets coordinate data from CSV file
     :return: List of lists of coords
     '''
-    with open('cities_subset20.csv', 'r') as f:
+    with open('cities_subset150.csv', 'r') as f:
+        print("reading...")
         reader = csv.reader(f)
         next(reader)
         city_coords = []
         for row in reader:
-            city_coords.append([float(row[1]), float(row[2])])
-        data = np.array(city_coords)
+            city_coords.append([int(float(row[1])), int(float(row[2]))])
+        data = city_coords
+    print("data acquired")
     return data
 
 
-def plot_coords():
-    '''
-    Plots all points in a scatterplot
-    :returns nothing:
-    '''
-    x, y = get_coords().T
-    plt.scatter(x, y)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
-    return None
+#def get_coordinates(data):
+    #result = []
+    #for i in data:
+        #result.append(i)
+    #print("coordinates acquired")
+    #return result
 
 
-plot_coords()
+def get_distance(coord_1, coord_2):
+    # Returns distance between two coordinates using Pythagoras' Equation
+    return round(((coord_1[0] - coord_2[0]) ** 2 + (coord_1[1] - coord_2[1]) ** 2) ** 0.5)
+
+
+def get_edges(coordinates):
+    # Gets all combinations of coordinates, get indices of them, and gets distance between the two.
+
+    combos = []
+    for i in coordinates:
+        for j in coordinates:
+            if i != j:
+                combos.append([coordinates.index(i), coordinates.index(j), get_distance(i, j)])
+    return combos
+
+print(get_data())
+print(get_edges(get_data()))
