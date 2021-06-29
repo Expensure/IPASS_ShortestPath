c16da8366a92267ead44c183e0a1064b271993ab
Expensure <jaspervanloon@outlook.com>
2021-06-29 16:43:12 +0200

    Created nice-looking plots

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-from get_data import *
+from get_data import get_data,import_file
 from Algorithm import algorithm
 from Clustering import cluster_main
-
+from two_opt_swap import run_alg
+from Town import Town
 
 def evaluate():
     '''Deprecated, may be used later'''
@@ -22,7 +23,7 @@ def evaluate():
     # Oud probleem: al 100 proberen te doen zorgt voor 100! combinaties oftewel 9.33*10^157 combinaties.
     # Nieuw plan: opsplitsen in enorme grid.
     # Beter plan: verminder naar 150 stuks. Gebruik data learning en k-means clustering om te vereenvoudigen.
-    coords = get_data()
+    coords = import_file("No")
     x1 = getMinimum(coords, 0)
     x2 = getMinimum(coords, 1)
     y1 = getMaximum(coords, 0)
@@ -42,45 +43,71 @@ def evaluate():
     return None ###result
 
 
-def plot_coords(coords, route):
-    import matplotlib.pyplot as plt
-    '''
-    Plots all points in a scatterplot
-    :returns nothing:
-    '''
+def plot_base(coords):
     x, y = [], []
     for i in get_data():
         x.append(i[0]), y.append(i[1])
+    plt.scatter(x,y)
+
+def plot_lines(route):
+    import matplotlib.pyplot as plt
+    '''
+    #Plots all points in a scatterplot
+    #:returns nothing:
+    #'''
+
     xl, yl = [], []
     for i in route:
-        xl.append(coords[i][0])
-        yl.append(coords[i][1])
+        xl.append(i[0])
+        yl.append(i[1])
     plt.plot(xl, yl)
     plt.xlabel('X')
     plt.ylabel('Y')
     return None
 
-def plot_tussen(coord_route):
-    for i in coord_route:
-        print(i)
-        plt.plot(i[0],i[1])
+def get_coords_of_towns(lst):
+    newlst = []
+    for i in lst:
+        newlst.append(Town.get_coords(i))
+    return newlst
 
-def main():
+def new_main():
     data = import_file("cities_subset150.csv")
-    clustered = cluster_main(data)
-    total = []
-    for i in clustered:
-        starts, ends = [], []
-        cluster = []
-        for j in i:
-            path,time = algorithm(j)
-            cluster.append(total)
-            plot_coords(j,path)
-            starts.append(j[path[0]])
-            ends.append(j[path[len(path)-1]])
-        plt.show()
-        total.append(cluster)
-        connections = algorithm_connect(starts,ends)
-        plot_tussen(connections)
+    plot_base(get_coords_of_towns(data))
     plt.show()
-main()
+    plot_lines(get_coords_of_towns(data))
+    plt.show()
+
+    two_opt,nearest_neighbours = run_alg(data)
+
+    plot_base(get_coords_of_towns(data))
+    plot_lines(get_coords_of_towns(nearest_neighbours))
+    plt.show()
+
+    plot_base(get_coords_of_towns(data))
+    plot_lines(get_coords_of_towns(two_opt))
+    plt.show()
+
+    plot_base(get_coords_of_towns(data))
+    plot_lines(get_coords_of_towns(nearest_neighbours))
+    plot_lines(get_coords_of_towns(two_opt))
+    plt.show()
+
+#def main():
+    #data = import_file("cities_subset150.csv")
+    #clustered = cluster_main(data)
+    #total = []
+    #for i in clustered:
+        #starts, ends = [], []
+        #cluster = []
+        #for j in i:
+            #path,time = algorithm(j)
+            #cluster.append(total)
+            #plot_coords(j,path)
+            #starts.append(j[path[0]])
+            #ends.append(j[path[len(path)-1]])
+        #plt.show()
+        #total.append(cluster)
+        #plot_tussen(connections)
+    #plt.show()
+new_main()
