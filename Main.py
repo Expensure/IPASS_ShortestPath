ed36c8f186e149807761a6709810ea1c775c46e3
Expensure <jaspervanloon@outlook.com>
2021-06-30 22:23:05 +0200

    Fixed up Main

--- Main.py
+++ Main.py
@@ -1,10 +1,7 @@
-import matplotlib.pyplot as plt
 from Clustering import cluster_main
 from Algorithm import run_alg
 from Town import Clustered_Town
 from tools import *
-
-
 
 
 def create_clustered_towns(clusters):
@@ -22,21 +19,6 @@ def create_clustered_towns(clusters):
             x += 1
         clustered_towns.append(cluster)
     return clustered_towns
-
-
-def test_main():
-    data = import_file("cities_100.csv")
-    coord_data = get_coords_of_towns(data)
-    clusters = cluster_main(coord_data, 2)
-    towns = create_clustered_towns(clusters)
-    two_time, _ = run_alg(data)
-    plot_lines(get_coords_of_towns(two_time))
-    plt.show()
-    plot_lines(get_coords_of_towns(two_time))
-    for i in towns:
-        two_opt, _ = run_alg(i)
-        plot_lines(get_coords_of_towns(two_opt))
-    plt.show()
 
 
 def new_main():
@@ -62,22 +44,44 @@ def new_main():
     plt.show()
 
 
-# def main():
-# data = import_file("cities_subset150.csv")
-# clustered = cluster_main(data)
-# total = []
-# for i in clustered:
-# starts, ends = [], []
-# cluster = []
-# for j in i:
-# path,time = algorithm(j)
-# cluster.append(total)
-# plot_coords(j,path)
-# starts.append(j[path[0]])
-# ends.append(j[path[len(path)-1]])
-# plt.show()
-# total.append(cluster)
-# plot_tussen(connections)
-# plt.show()
+def test_main():
+    """
+    Deprecated
+    """
+    data = import_file("cities_100.csv")
+    coord_data = get_coords_of_towns(data)
+    clusters = cluster_main(coord_data, 2)
+    towns = create_clustered_towns(clusters)
+    two_time, _ = run_alg(data)
+    plot_lines(get_coords_of_towns(two_time))
+    plt.show()
+    plot_lines(get_coords_of_towns(two_time))
+    for i in towns:
+        two_opt, _ = run_alg(i)
+        plot_lines(get_coords_of_towns(two_opt))
+    plt.show()
+
+
+def main():
+    """
+    Deprecated
+    """
+    data = import_file("cities_subset150.csv")
+    clustered = cluster_main(data, 4)
+    total = []
+    for i in clustered:
+        starts, ends = [], []
+        cluster = []
+        for j in i:
+            path, time = run_alg(j)
+            cluster.append(total)
+            # plot_coords(j,path)
+            starts.append(j[path[0]])
+            ends.append(j[path[len(path) - 1]])
+            plt.show()
+        total.append(cluster)
+        # plot_tussen(connections)
+    plt.show()
+
 
 new_main()
